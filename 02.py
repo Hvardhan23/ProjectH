84ace5db57579c67ba1cf0d5d0d8752587aaa83d
Hvardhan23 <88668621+Hvardhan23@users.noreply.github.com>
2026-08-25 19:13:49 +0530

    ....

--- 02.py
+++ 02.py
@@ -1,1 +1,22 @@
+def receive():
+    print(f"Server is listening on {HOST}:{PORT}...")
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
 
+        # Prompt client for a nickname
+        client.send('NICK'.encode('utf-8'))
+        nickname = client.recv(1024).decode('utf-8')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname of client is {nickname}!")
+        broadcast(f"{nickname} joined the chat!\n".encode('utf-8'))
+        client.send('Connected to the server!'.encode('utf-8'))
+
+        # Start a thread to manage this unique client connection
+        thread = threading.Thread(target=handle_client, args=(client,))
+        thread.start()
+
+if __name__ == "__main__":
+    receive()
